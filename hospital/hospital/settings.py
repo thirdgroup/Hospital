8cd346de21af3b080ba383060334f160a89bddad
zhaoxinrong <1473233993@qq.com>
2019-04-30 14:23:19 +0800

    创建了requirements.txt和Install了rest_framework

--- hospital/hospital/settings.py
+++ hospital/hospital/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'database.apps.DatebaseConfig',
     'drugmanage.apps.DrugmanageConfig',
     'inhospital.apps.InhospitalConfig',
